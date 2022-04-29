47c7bd32ef01ce7fffa66be35ba96490eb299a08
Gus Teran <ateran@wpi.edu>
2022-04-29 12:50:23 -0400

    Make the left motor turn
    The right motor does not turn still

--- Capstone PCB Swarm/main.c
+++ Capstone PCB Swarm/main.c
@@ -23,20 +23,20 @@ volatile bool LED_ON = false;
 #define IR_LED PB7
 
 // TODO: Implement the micros function for myself
-inline static unsigned long micros() { return microseconds; }
+//inline static unsigned long micros() { return microseconds; }
 
-inline void setLED() {
+void setLED() {
     PORTB |= 1 << IR_LED;
 }
 
-inline void unsetLED() {
+void unsetLED() {
     PORTB &= ~(1 << IR_LED);
 }
-inline void stopIR() {
+void stopIR() {
     TCCR2 = 0x00;
 }
 
-inline void startIR() {
+void startIR() {
     // Sets to CTC
     TCCR2 = (1 << WGM21) | (1 << COM20) | (1 << CS20);
 }
@@ -50,7 +50,7 @@ inline void setupIR() {
     TCNT2 = 0;
 }
 
-inline void setupTimer() {
+void setupTimer() {
 	OCR0 = 100;
 	TCNT0 = 0;
 	TCCR0 = (1 << WGM01) | (1 << CS01);
@@ -69,6 +69,53 @@ ISR(TIMER0_COMP_vect) {
 	//microseconds++;
 }
 
+//define a drive variable for the output compare register to turn on when reached
+byte drive = 0;
+
+#define LEFT_DC PE3
+#define RIGHT_DC PE4
+
+#define LEFT_MOTOR PE6
+#define RIGHT_MOTOR PE7
+
+#define MOTOR_ENABLE PG3
+
+
+
+void setupDC() {
+	//pre scaler set to (1)
+	TCCR3B = (1 << WGM32) | (1 << CS30);
+	
+	//pwm ramp up only mode (count TCNT3 up to OCR3A then stop); 8-bit
+	TCCR3A = (1 << WGM30) | (1 << COM3A1) | (1 << COM3A1);
+	
+	//define counter pre-set value (this is what we start counting up from)
+	TCNT3 = 0;
+	
+	//define value to count up to (no pre scaler used so can be from 1 to 254)
+	OCR3A = 200;
+
+	//the closer TCNT3 is set to 200, the closer we are to 100% duty cycle
+	
+	//set pins 5 and 6 (PE3 and PE4) only as output
+	//DDRE= 0b00011000;
+	DDRE |= (1 << LEFT_DC) | (1 << RIGHT_DC);
+	
+}
+
+void driveMotors() {
+	//PORTE |= (1 << LEFT_MOTOR ); 
+	//PORTE |= (1 << RIGHT_MOTOR);
+	PORTG |= (1 << MOTOR_ENABLE);
+}
+
+
+void stopMotors() {
+	//PORTE &= ~(1 << LEFT_MOTOR ); 
+	//PORTE &= ~(1 << RIGHT_MOTOR);
+	PORTG &= ~(1 << MOTOR_ENABLE);
+}
+
 int main(void) {
 	microseconds = 0;
 	unsetLED();
@@ -77,11 +124,12 @@ int main(void) {
 	_delay_ms(500);
 	unsetLED();
     setupIR();
-    setupTimer();
 	_delay_ms(500);
 	stopIR();
 	sei();
 	DDRB = (1 << IR_LED);
+	setupTimer();
+	setupDC();
     while (1) {
 		//cli();
 		//if(microseconds > 10){
@@ -89,6 +137,10 @@ int main(void) {
 			//microseconds = 0;
 		//}
 		//sei();
+		driveMotors();
+		_delay_ms(2000);
+		stopMotors();
+		_delay_ms(500);
     }
     return 1;
 }
